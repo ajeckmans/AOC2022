d9ee932f7d329f199d20d5d50a7f460f8bac0bb0
ajeckmans <ajeckmans@users.noreply.github.com>
2022-12-12 00:02:00 +0100

    Day 3

--- AOC2022.Runner/AOC.fs
+++ AOC2022.Runner/AOC.fs
@@ -1,14 +1,13 @@
 namespace AOC2022.Runner.AOC
 
+open System
 open System.IO
 open FsHttp
 
 module AOC =
     let getSampleInput day puzzle =
         let fileName = $"./content/{day}_{puzzle}_sample"
-        task {
-            return! (File.ReadAllTextAsync fileName)
-        }
+        task { return! (File.ReadAllTextAsync fileName) }
 
     let downloadInput day puzzle session =
         task {
@@ -24,24 +23,34 @@ module AOC =
                         CacheControl "no-cache"
                     }
                     |> Request.sendTAsync
-                
+
                 let! content = response.content.ReadAsStringAsync()
-             
+
+                let sanitized = content.TrimEnd('\n')
+
                 Directory.CreateDirectory("content") |> ignore
-                do! File.WriteAllTextAsync(fileName, content)
-                
-                return content
+                do! File.WriteAllTextAsync(fileName, sanitized)
+
+                return sanitized
         }
 
     let submitInput day puzzle input session =
-        http {
-            POST $"https://adventofcode.com/2022/day/{day}/answer"
-            CacheControl "no-cache"
-            Cookie "session" session
-            body
-            formUrlEncoded [
-                    "level", puzzle |> string
-                    "answer", input |> string
-                ]
+        task {
+            let! response =
+                http {
+                    POST $"https://adventofcode.com/2022/day/{day}/answer"
+                    CacheControl "no-cache"
+                    Cookie "session" session
+                    body
+                    formUrlEncoded [ "level", puzzle |> string; "answer", input |> string ]
+                }
+                |> Request.sendTAsync
+
+            response.originalHttpResponseMessage.EnsureSuccessStatusCode() |> ignore
+            let! responseContent = response.content.ReadAsStringAsync()
+
+            if responseContent.Contains("That's the right answer!", StringComparison.OrdinalIgnoreCase) then
+                return Ok "Answer is correct"
+            else
+                return Error responseContent
         }
-        |> Request.sendTAsync
---
+++ AOC2022.Runner/Puzzles/day3.fs
@@ -0,0 +1,64 @@
+module AOC2022.Runner.Puzzles.Day2
+
+open System
+
+
+type Outcome =
+    | Win
+    | Loss
+    | Draw
+
+let runRoundLine scoring (line: string) =
+    if String.IsNullOrEmpty line then
+        0
+    else
+        let x = line.Split(" ")
+        scoring x[0] x[1]
+
+let runPuzzle_2_1 (input: string) =
+    let getScore x y =
+        match y with
+        | "X" ->
+            match x with
+            | "A" -> Draw, 1
+            | "B" -> Loss, 1
+            | "C" -> Win, 1
+            | _ -> failwith "invalid hand"
+        | "Y" ->
+            match x with
+            | "A" -> Win, 2
+            | "B" -> Draw, 2
+            | "C" -> Loss, 2
+            | _ -> failwith "invalid hand"
+        | "Z" ->
+            match x with
+            | "A" -> Loss, 3
+            | "B" -> Win, 3
+            | "C" -> Draw, 3
+            | _ -> failwith "invalid hand"
+        | _ -> failwith "invalid hand"
+
+    let runRound opponent self =
+        match getScore opponent self with
+        | Loss, s -> s
+        | Win, s -> s + 6
+        | Draw, s -> s + 3
+
+    input.Split("\n") |> Seq.map (runRoundLine runRound) |> Seq.sum
+
+let runPuzzle_2_2 (input: string) =
+    let getScore opponent result =
+        let opponentScore =
+            match opponent with
+            | "A" -> 1
+            | "B" -> 2
+            | "C" -> 3
+            | _ -> failwith "invalid hand"
+        
+        match result with
+        | "X" -> (if opponentScore = 1 then 3 else opponentScore - 1)
+        | "Y" -> opponentScore + 3
+        | "Z" -> (if opponentScore = 3 then 1 else opponentScore + 1) + 6
+        | _ -> failwith "invalid hand"
+
+    input.Split("\n") |> Seq.map (runRoundLine getScore) |> Seq.sum
